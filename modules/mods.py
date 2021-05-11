1e71c3bbbb8386c82145986bdc1a8593c8d5e580
Shark1ight <shark1ight@naver.com>
2021-05-12 07:12:01 +0900

    Update mods.py

--- modules/mods.py
+++ modules/mods.py
@@ -21,7 +21,7 @@ class Moderation(commands.Cog, name="모드"):
                     ctx,
                     member: discord.Member,
                     *,
-                    reason: typing.Optional[str] = "사유 없음."):
+                    reason: typing.Optional[str] = "사유가 지정되지 않았어요."):
         """
         미야야 뮤트 < @유저 > [ 사유 ]
 
@@ -40,14 +40,14 @@ class Moderation(commands.Cog, name="모드"):
                 else:
                     await member.add_roles(role, reason=reason)
                     await ctx.reply(
-                        f"<:mute:761151751583301682> **{member}**님을 뮤트했어요.\n사유 : {reason}"
+                        f"<:mute:761151751583301682> **{member}**님을 뮤트했어요.\n사유 : *{reason}*"
                     )
             else:
                 await ctx.reply(
                     f"<:mute:761151751583301682> 그 유저는 이미 뮤트되어 있어요!")
         else:
             await ctx.reply(
-                f"<:cs_no:659355468816187405> 이 명령어를 실행하려면 뮤트에 사용할 역할이 지정되어 있어야 해요. `미야야 뮤트설정` 명령어를 사용해 설정을 완료해주세요."
+                f"<:cs_no:659355468816187405> 이 명령어를 실행하려면 뮤트에 사용할 역할이 지정되어 있어야 해요. `미야야 뮤트설정 `@역할`` 명령어를 사용해 설정을 완료해주세요."
             )
 
     @commands.command(name="언뮤트")
@@ -57,7 +57,7 @@ class Moderation(commands.Cog, name="모드"):
                       ctx,
                       member: discord.Member,
                       *,
-                      reason: typing.Optional[str] = "사유 없음."):
+                      reason: typing.Optional[str] = "사유가 지정되지 않았어요."):
         """
         미야야 언뮤트 < @유저 > [ 사유 ]
 
@@ -77,7 +77,7 @@ class Moderation(commands.Cog, name="모드"):
                 await ctx.reply(f"<:mic:761152232447148042> 그 유저는 뮤트되지 않았어요!")
         else:
             await ctx.reply(
-                f"<:cs_no:659355468816187405> 이 명령어를 실행하려면 뮤트에 사용할 역할이 지정되어 있어야 해요. `미야야 뮤트설정` 명령어를 사용해 설정을 완료해주세요."
+                f"<:cs_no:659355468816187405> 이 명령어를 실행하려면 뮤트에 사용할 역할이 지정되어 있어야 해요. `미야야 뮤트설정 `@역할`` 명령어를 사용해 설정을 완료해주세요."
             )
 
     @commands.command(name="슬로우", aliases=["슬로우모드"])
@@ -110,7 +110,7 @@ class Moderation(commands.Cog, name="모드"):
                     ctx,
                     member: discord.Member,
                     *,
-                    reason: typing.Optional[str] = "사유 없음."):
+                    reason: typing.Optional[str] = "사유가 지정되지 않았어요."):
         """
         미야야 추방 < 유저 > [ 사유 ]
 
@@ -123,7 +123,7 @@ class Moderation(commands.Cog, name="모드"):
                     f"<a:ban_cat:761149577444720640> **{ctx.guild.name}** 서버에서 추방당하셨어요.\n추방한 관리자 : {ctx.author}\n사유 : {reason}"
                 )
             except:
-                print("Kick DM Failed.")
+                pass
             await ctx.guild.kick(member, reason=reason)
             await ctx.reply(
                 f"<a:ban_cat:761149577444720640> **{member}**님을 서버에서 추방했어요!\n사유 : {reason}"
@@ -142,7 +142,7 @@ class Moderation(commands.Cog, name="모드"):
         user: discord.Member,
         delete: typing.Optional[int] = 0,
         *,
-        reason: typing.Optional[str] = "사유 없음.",
+        reason: typing.Optional[str] = "사유가 지정되지 않았어요.",
     ):
         """
         미야야 차단 < 유저 > [ 메시지 삭제 일 수 ( ~7일까지 ) ] [ 사유 ]
@@ -156,7 +156,7 @@ class Moderation(commands.Cog, name="모드"):
                     f"<a:ban_guy:761149578216603668> {ctx.author.mention} **{ctx.guild.name} 서버에서 영구적으로 차단당하셨어요.\n차단한 관리자 : {ctx.author}\n사유 : {reason}"
                 )
             except:
-                print("Ban DM Failed.")
+                pass
             await ctx.guild.ban(user,
                                 delete_message_days=delete,
                                 reason=reason)
