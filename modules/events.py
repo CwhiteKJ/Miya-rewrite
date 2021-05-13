8fb0f2c89582e1265c0b605dc278e26228f79895
LEE WON JAE <shark1ight@naver.com>
2021-05-13 15:28:59 +0900

    Move answering feature to event listener

--- modules/events.py
+++ modules/events.py
@@ -1,13 +1,13 @@
-import asyncio
 import datetime
-import json
 import locale
+import random
 
 import aiohttp
 import discord
 from discord.ext import commands
 
 from lib import utils
+from lib import config
 from lib.utils import Forbidden
 from lib.utils import Maintaining
 from lib.utils import NoReg
@@ -16,6 +16,7 @@ from lib.utils import sql
 locale.setlocale(locale.LC_ALL, "")
 
 Hook = utils.Hook()
+Check = utils.Check()
 
 
 class Listeners(commands.Cog, name="이벤트 리스너"):
@@ -73,13 +74,88 @@ class Listeners(commands.Cog, name="이벤트 리스너"):
             "manage_webhooks": "웹훅 관리하기",
             "manage_messages": "메시지 관리하기",
         }
-        if isinstance(error, commands.CommandNotFound) or isinstance(
-                error, commands.NotOwner):
-            return
-        if isinstance(error, Forbidden):
-            await ctx.reply(str(error), embed=error.embed)
-        elif isinstance(error, NoReg) or isinstance(error, Maintaining):
-            await ctx.reply(str(error))
+        if (isinstance(error, commands.CommandNotFound)
+                or isinstance(error, commands.NotOwner)
+                or isinstance(error, commands.CheckFailure)):
+            try:
+                p = await Check.identify(ctx)
+            except Exception as e:
+                if isinstance(e, Forbidden):
+                    await ctx.reply(str(e), embed=e.embed)
+                elif isinstance(e, NoReg) or isinstance(e, Maintaining):
+                    await ctx.reply(str(e))
+                elif isinstance(e, commands.NoPrivateMessage):
+                    return
+            else:
+                if p is True:
+                    headers = {
+                        "Authorization": config.PPBToken,
+                        "Content-Type": "application/json",
+                    }
+                    query = ctx.message.content.replace("미야야 ", "")
+                    query2 = query.replace(" ", "")
+                    query2.replace("\\", "")
+                    query2.replace('"', "")
+                    query2.replace("'", "")
+                    # query2.tolower()
+                    embed = None
+                    rows = await sql(
+                        0, f"SELECT * FROM `cc` WHERE `word` = '{query2}'")
+                    if not rows:
+                        async with aiohttp.ClientSession() as cs:
+                            async with cs.post(
+                                    config.PPBRequest,
+                                    headers=headers,
+                                    json={"request": {
+                                        "query": query
+                                    }},
+                            ) as r:
+                                response_msg = await r.json()
+                                msg = response_msg["response"]["replies"][0][
+                                    "text"]
+                                if (msg !=
+                                        "앗, 저 이번 달에 할 수 있는 말을 다 해버렸어요 🤐 다음 달까지 기다려주실거죠? ☹️"
+                                    ):
+                                    await Hook.terminal(
+                                        0,
+                                        f"PINGPONG Builder >\nUser - {ctx.author} ({ctx.author.id})\nSent - {query}\nReceived - {msg}\nGuild - {ctx.guild.name} ({ctx.guild.id})",
+                                        "명령어 처리 기록",
+                                        self.miya.user.avatar_url,
+                                    )
+                                    embed = discord.Embed(
+                                        title=msg,
+                                        description=
+                                        f"[Discord 지원 서버 접속하기](https://discord.gg/tu4NKbEEnn)\n[한국 디스코드 봇 리스트 하트 누르기](https://koreanbots.dev/bots/720724942873821316)",
+                                        color=0x5FE9FF,
+                                    )
+                                    embed.set_footer(
+                                        text=
+                                        "이 답변은 https://pingpong.us/를 통해 만들어졌습니다."
+                                    )
+                                else:
+                                    embed = discord.Embed(
+                                        title="💭 이런, 미야가 말풍선을 모두 사용한 모양이네요.",
+                                        description=
+                                        f"매월 1일에 말풍선이 다시 생기니 그 때까지만 기다려주세요!\n \n[Discord 지원 서버 접속하기](https://discord.gg/tu4NKbEEnn)\n[한국 디스코드 봇 리스트 하트 누르기](https://koreanbots.dev/bots/720724942873821316)",
+                                        color=0x5FE9FF,
+                                    )
+                                    embed.set_footer(
+                                        text=
+                                        "이 답변은 https://pingpong.us/를 통해 만들어졌습니다."
+                                    )
+                    else:
+                        row = random.choice(rows)
+                        user = self.miya.get_user(int(row[3]))
+                        embed = discord.Embed(
+                            title=row[2],
+                            description=
+                            f"[Discord 지원 서버 접속하기](https://discord.gg/tu4NKbEEnn)\n[한국 디스코드 봇 리스트 하트 누르기](https://koreanbots.dev/bots/720724942873821316)",
+                            color=0x5FE9FF,
+                        )
+                        embed.set_footer(
+                            text=
+                            f"이 답변은 {user.name}({row[0]})님의 지식을 통해 만들어졌습니다.")
+                    await ctx.reply(embed=embed)
         elif isinstance(error, discord.NotFound) or isinstance(
                 error, commands.NoPrivateMessage):
             return
